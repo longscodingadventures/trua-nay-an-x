3b9ae05cc14fc7cea77abe9d4025f99026776178
longscodingadventures <63569684+longscodingadventures@users.noreply.github.com>
2022-08-02 15:16:31 +0700

    last ever commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# trua-nay-an-x
+## `has this ever happened to you?`
+
+> ê m trưa nay ăn gì
+> ai bic đou tr
+
+
+1 cuộc đối thoại quá đỗi quen thuộc với học sinh chuyên bắc phoi
+
+đừng lo, công ty giải pháp công nghệ longscodingadventures LLC xin giới thiệu sản phẩm mới nhất của chúng tôi:
+
+# `trưa-nay-ăn-x` (x: một món ăn)
  
+
+sản phẩm áp dụng công nghệ "trí tuệ tự nhiên" độc quyền, giúp chọn món ăn tốt nhất cho hoàn cảnh
+
+disclaimer: tác giả không chịu trách nhiệm cho lựa chọn của `trưa-nay-ăn-x`
+
+This software and all dependencies use the `MIT license`. For more information, refer to `LICENSE`
--- main.py
+++ main.py
@@ -3,24 +3,24 @@ import time
 
 # vanity
 print('=======================================')
-print('             trua nay an gi            ')
-print('             (c) 2022 long             ')
+print('           Trua nay an gi (tm)         ')
+print('             (c) 2022 Long             ')
 print('=======================================')
 
 # txt to py list
 tnag_file = open("tnag.txt", "r")
 trua_nay_an_gi = tnag_file.read()
 tnag_list = trua_nay_an_gi.split("\n")
+tnag_list.pop(0)
 
 # debug
-print("danh sách có số item :", + len(tnag_list))
+print('Danh sách có {} items'.format(len(tnag_list)))
 
 # "artificial" "intelligence"
 print('please wait while the AI leverages choices')
-time.sleep(5)
-
-# random
-random_item = random.choice(tnag_list)
-print("Hôm nay ăn", random_item)
+time.sleep(2)
+print('almost done...')
+time.sleep(3)
+print("Hôm nay ăn {}".format(random.choice(tnag_list)))
 
 tnag_file.close()
