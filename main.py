5134b0a8fae2440a75ee396fe8def40b9037c291
longscodingadventures <63569684+longscodingadventures@users.noreply.github.com>
2022-08-02 09:06:55 +0700

    added vanity and "AI"

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import random
+import time
+
+# vanity
+print('=======================================')
+print('             trua nay an gi            ')
+print('             (c) 2022 long             ')
+print('=======================================')
 
 # txt to py list
 tnag_file = open("tnag.txt", "r")
@@ -6,7 +13,11 @@ trua_nay_an_gi = tnag_file.read()
 tnag_list = trua_nay_an_gi.split("\n")
 
 # debug
-print("danh sách có số item : ", + len(tnag_list))
+print("danh sách có số item :", + len(tnag_list))
+
+# "artificial" "intelligence"
+print('please wait while the AI leverages choices')
+time.sleep(5)
 
 # random
 random_item = random.choice(tnag_list)
